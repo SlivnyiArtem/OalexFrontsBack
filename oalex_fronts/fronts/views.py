40a93de3b9c6ad2a0cc3f235793c93008b2fd762
flamberg <slivnay1965@gmail.com>
2025-12-24 18:02:01 +0500

    time window for form

--- oalex_fronts/fronts/views.py
+++ oalex_fronts/fronts/views.py
@@ -1,27 +1,69 @@
 from django.shortcuts import render
 from django.views import View
+from datetime import datetime
 
 from fronts.utils import build_multi_time_citation_graph
 
 
+def _get_default_time_windows():
+    return ["2023-01-01", "2023-12-31", "2024-12-31"]
+
+
 class HomepageFormView(View):
     template_name = 'formreq.html'
-    DEFAULT_TIME_WINDOWS = ["2023-01-01", "2023-12-31", "2024-12-31"]
 
     def get(self, request):
+        default_windows = _get_default_time_windows()
+
         return render(request, self.template_name, {
-            'time_windows': self.DEFAULT_TIME_WINDOWS
+            'time_windows': default_windows,
+            'window1': default_windows[0] if len(default_windows) > 0 else '',
+            'window2': default_windows[1] if len(default_windows) > 1 else '',
+            'window3': default_windows[2] if len(default_windows) > 2 else ''
         })
 
     def post(self, request):
         theme_id = request.POST.get('theme_id')
 
-        time_windows_list = self.DEFAULT_TIME_WINDOWS
+        window1 = request.POST.get('window1', '')
+        window2 = request.POST.get('window2', '')
+        window3 = request.POST.get('window3', '')
+
+        time_windows_list = []
+        if window1:
+            time_windows_list.append(window1)
+        if window2:
+            time_windows_list.append(window2)
+        if window3:
+            time_windows_list.append(window3)
+
+        if not time_windows_list:
+            time_windows_list = _get_default_time_windows()
+
+        try:
+            dates = [datetime.strptime(date, '%Y-%m-%d') for date in time_windows_list]
+            if dates != sorted(dates):
+                return render(request, self.template_name, {
+                    'error': 'Даты должны быть в хронологическом порядке',
+                    'theme_id': theme_id,
+                    'window1': window1,
+                    'window2': window2,
+                    'window3': window3,
+                    'time_windows': time_windows_list
+                })
+        except ValueError as e:
+            return render(request, self.template_name, {
+                'error': f'Некорректный формат даты: {str(e)}',
+                'theme_id': theme_id,
+                'window1': window1,
+                'window2': window2,
+                'window3': window3,
+                'time_windows': time_windows_list
+            })
 
         time_graph, all_clusters, citation_matrices = build_multi_time_citation_graph(
             time_windows_list, theme_id, K=5, min_cluster_size=2
         )
-
 
         nodes_data = list(time_graph.nodes(data=True))
         edges_data = []
@@ -35,7 +77,6 @@ class HomepageFormView(View):
                 'size_q': data.get('size_q', 0)
             }
             edges_data.append(edge_info)
-
 
         window_stats = {}
         for node, data in nodes_data:
@@ -52,9 +93,6 @@ class HomepageFormView(View):
         top_edges = sorted(edges_data, key=lambda x: x['weight'], reverse=True)[:5]
 
         time_window_range = range(1, len(time_windows_list))
-
-        # visualize_time_graph(time_graph)
-
 
         return render(request, 'homepage_results.html', {
             'theme_id': theme_id,
